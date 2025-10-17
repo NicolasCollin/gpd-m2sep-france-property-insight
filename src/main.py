bedabf9da04bd87c3af11b5288ccd8f3de76c1fa
Nicolas COLLIN <nicolas.collin@etudiant.univ-reims.fr>
2025-10-17 16:25:40 +0200

    ajout de commentaires explicatifs dans le code principal

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
-from src.interface.menu import app_menu
+from src.interface.menu import app_menu  # Import menu
 
 
 def get_welcome_message() -> str:
-    return "Welcome to FPI app!"
+    return "Welcome to FPI app!"  # Welcome message
 
 
 def main():
-    app = app_menu()
-    app.launch()
+    app = app_menu()  # Create app
+    app.launch()  # Run app
 
 
 if __name__ == "__main__":
-    main()
+    main()  # Entry point
