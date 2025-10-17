5ed75a73e7a7dc54a2e35f9c01d4e3b397ca18f2
Nicolas COLLIN <nicolas.collin@etudiant.univ-reims.fr>
2025-10-17 16:27:51 +0200

    ajout de commentaires explicatifs dans les fonctions d'alias et oranj

--- src/utils/oranj.py
+++ src/utils/oranj.py
@@ -1,3 +1,3 @@
-def favourite_colour() -> None:
-    colour: str = input("what is your favourite colour ?\n")
-    print(f"Nice, my favourite colours is also {colour}.")
+def favourite_colour() -> None:  # Ask user for favourite colour
+    colour: str = input("what is your favourite colour ?\n")  # Get user input
+    print(f"Nice, my favourite colours is also {colour}.")  # Display message
