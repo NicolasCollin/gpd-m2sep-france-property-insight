aa89ae6e443092cd94e60aa7d50445bc21e7f59f
phan0005 <daniel.phan@etudiant.univ-reims.fr>
2025-10-17 08:42:39 +0200

    added sample data, aliases, updated docker setup for new main

--- src/utils/aliases.py
+++ src/utils/aliases.py
@@ -6,20 +6,24 @@ def precommit() -> None:
     subprocess.run(["uv", "run", "pre-commit", "run", "--all-files"], check=False)
 
 
-def fpibuild() -> None:
-    """uv run fpibuild shortcut to build and run fpi app with Docker"""
+def fpi() -> None:
+    """uv run fpi shortcut to build and run fpi app with Docker"""
     subprocess.run(
-        [
-            "uv",
-            "run",
-            "docker",
-            "compose",
-            "-f",
-            ".devcontainer/compose.yaml",
-            "run",
-            "--rm",
-            "-it",
-            "server",
-        ],
+        ["uv", "run", "docker", "compose", "-f", ".devcontainer/compose.yaml", "up", "--build"],
         check=False,
     )
+
+
+def typecheck() -> None:
+    """uv run typecheck to run mypy tests"""
+    subprocess.run(["uv", "run", "mypy", "src"])
+
+
+def audit() -> None:
+    """uv run audit to run pip-audit vuln checks"""
+    subprocess.run(["uv", "run", "pip-audit", "."])
+
+
+def test() -> None:
+    """uv run test to run our tests"""
+    subprocess.run(["uv", "run", "pytest", "--doctest-modules"])
