8b2fd1076205f021c3d7ce47ebe46b8e3d272e8b
phan0005 <daniel.phan@etudiant.univ-reims.fr>
2025-10-21 19:26:59 +0200

    added bp_analysis

---
+++ fpi/analysis/utils_io.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def print_info(df: pd.DataFrame) -> None:
+    """Print basic dataset information."""
+    print("\n===== HEAD =====")
+    print(df.head(), "\n")
+
+    print("===== INFO =====")
+    # print(df.info(), "\n")\
+    df.info()
+
+    print(f"\nShape: {df.shape[0]} rows × {df.shape[1]} columns\n")
+
+    missing = df.isnull().sum()
+    missing = missing[missing > 0]
+    if not missing.empty:
+        print("Missing values:\n", missing, "\n")
