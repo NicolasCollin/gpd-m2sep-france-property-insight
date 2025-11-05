2cadf8f4c04ad5dcbc7117bbca3a016501e33044
phan0005 <daniel.phan@etudiant.univ-reims.fr>
2025-11-05 14:46:15 +0100

    fix(constants): update VARS_TO_KEEP added transaction_date, town_name, property_type

--- fpi/utils/constants.py
+++ fpi/utils/constants.py
@@ -7,11 +7,14 @@ from typing import List
 
 # Columns to keep in cleaned CSV / processed data
 VARS_TO_KEEP: List[str] = [
+    "transaction_date",
     "property_value",
     "postal_code",
+    "town_name",
     "department_code",
     "town_code",
     "property_type_code",
+    "property_type",
     "building_area",
     "main_rooms",
     "land_area",
