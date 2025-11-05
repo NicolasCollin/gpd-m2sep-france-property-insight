15e6508d8057861f81eb08e9c895c314e41125fb
nday0006 <perle-marie-salma.ndayizeye@etudiant.univ-reims.fr>
2025-11-05 15:17:15 +0100

    tests succeeded

--- tests/unit/test_analysis/test_explore.py
+++ tests/unit/test_analysis/test_explore.py
@@ -1,6 +1,9 @@
+import shutil
+import unittest
+from pathlib import Path
 from typing import List
 
-from fpi.analysis.explore import load_data
+from fpi.analysis.explore import display_trend, load_data
 from fpi.utils.constants import VARS_TO_KEEP
 
 
@@ -22,3 +25,23 @@ class TestLoadData:
         expected_cols: List[str] = VARS_TO_KEEP
         for col in expected_cols:
             assert col in self.clean_df.columns
+
+
+class TestExploreModule(unittest.TestCase):
+    def setUp(self):
+        """creates a temporary folder which stocks the test files."""
+        self.temp_dir = Path("tests/temp_plots")
+        self.temp_dir.mkdir(parents=True, exist_ok=True)
+
+    def tearDown(self):
+        """deletes all the PNG files created by the tests"""
+        if self.temp_dir.exists():
+            shutil.rmtree(self.temp_dir)
+
+    def test_display_trend(self):
+        """Verify if display_trend creates graphs"""
+        display_trend("data/cleaned", dept_filter="75", output_dir=self.temp_dir)
+        display_trend("data/cleaned", dept_filter="92", output_dir=self.temp_dir)
+
+        png_files = list(self.temp_dir.glob("*.png"))
+        self.assertGreater(len(png_files), 0, "No PNG file generated.")
